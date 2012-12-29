bb566f510f78fc2b80f6e55ea4b1fdb013d0e90d
pbrier <git@pbrier.nl>
2012-12-29 16:18:34 +0100

    Added focus scan

--- lua/powerscan.lua
+++ lua/powerscan.lua
@@ -4,6 +4,15 @@ require "laos";
 xpos = 100;
 ypos = 100;
 
+-- focus scan
+laos.speed(50);
+laos.power(50);
+for z = -10,10 do
+--  x = xpos + 10 * z;
+--  laos.move(nil, nil, z);
+--  laos.box(x, ypos-20, 5,5);
+end;
+
 for x = 1,10 do
   for y = 1,10 do
     laos.speed(10*x);
